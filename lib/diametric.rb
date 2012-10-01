b69ac5d06b592a573b1681c6ba9697d422ac978b
Clinton N. Dreisbach <crnixon@gmail.com>
2012-10-01 11:50:29 -0400

    Naively generates schemas

--- lib/diametric.rb
+++ lib/diametric.rb
@@ -1,18 +1,74 @@
 require "diametric/version"
+require "bigdecimal"
 
 module Diametric
+  VALUE_TYPES = {
+    Symbol => "keyword",
+    String => "string",
+    Integer => "long",
+    Float => "float",
+    BigDecimal => "bigdec",
+    DateTime => "instant",
+    URI => "uri"
+  }
+
   def self.included(base)
     base.send(:include, InstanceMethods)
     base.send(:extend, ClassMethods)
 
     # TODO set up :db/id
+    base.class_eval do
+      @attrs = []
+      @part = :"db.part/db"
+    end
   end
 
   module ClassMethods
-    def attr(name, opts)
+    def tempid(part)
+    end
+
+    def namespace(ns, val)
+      [ns.to_s, val.to_s].join("/").to_sym
+    end
+
+    def value_type(vt)
+      if vt.is_a?(Class)
+        vt = Diametric::VALUE_TYPES[vt]
+      end
+      namespace("db.type", vt)
+    end
+
+    def prefix
+      self.to_s.downcase
+    end
+
+    def attr(name, value_type, opts = {})
+      @attrs << [name, value_type, opts]
     end
 
     def schema
+      defaults = {
+        :"db/id" => tempid(@part),
+        :"db/cardinality" => :"db.cardinality/one",
+        :"db.install/_attribute" => @part
+      }
+
+      @attrs.reduce([]) do |schema, (attr, value_type, opts)|
+        unless opts.empty?
+          opts[:cardinality] = namespace("db.cardinality", opts[:cardinality]) if opts[:cardinality]
+          opts[:unique] = namespace("db.unique", opts[:unique]) if opts[:unique]
+          opts = opts.map { |k, v|
+            k = namespace("db", k)
+            [k, v]
+          }
+          opts = Hash[*opts.flatten]
+        end
+
+        schema << defaults.merge({
+          :"db/ident" => namespace(prefix, attr),
+          :"db/valueType" => value_type(value_type),
+        }).merge(opts)
+      end
     end
 
     def query(params)
--- spec/diametric_spec.rb
+++ spec/diametric_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 class Person
   include Diametric
 
-  attr :name, :index => true
+  attr :name, String, :index => true
+  attr :email, String, :cardinality => :many
+  attr :birthday, DateTime
+  attr :awesome, :boolean, :doc => "Is this person awesome?"
+  attr :ssn, String, :unique => :value
+  attr :secret_name, String, :unique => :identity
+  attr :bio, String, :fulltext => true
 end
 
 describe Diametric do
@@ -11,9 +17,55 @@ describe Diametric do
     subject { Person }
 
     it { should respond_to(:attr) }
+    it { should respond_to(:tempid) }
     it { should respond_to(:schema) }
     it { should respond_to(:query) }
     it { should respond_to(:from_query) }
+
+    it "should generate a schema" do
+      Person.schema.should == [
+        { :"db/id" => Person.tempid(:"db.part/db"),
+          :"db/ident" => :"person/name",
+          :"db/valueType" => :"db.type/string",
+          :"db/cardinality" => :"db.cardinality/one",
+          :"db/index" => true,
+          :"db.install/_attribute" => :"db.part/db" },
+        { :"db/id" => Person.tempid(:"db.part/db"),
+          :"db/ident" => :"person/email",
+          :"db/valueType" => :"db.type/string",
+          :"db/cardinality" => :"db.cardinality/many",
+          :"db.install/_attribute" => :"db.part/db" },
+        { :"db/id" => Person.tempid(:"db.part/db"),
+          :"db/ident" => :"person/birthday",
+          :"db/valueType" => :"db.type/instant",
+          :"db/cardinality" => :"db.cardinality/one",
+          :"db.install/_attribute" => :"db.part/db" },
+        { :"db/id" => Person.tempid(:"db.part/db"),
+          :"db/ident" => :"person/awesome",
+          :"db/valueType" => :"db.type/boolean",
+          :"db/cardinality" => :"db.cardinality/one",
+          :"db/doc" => "Is this person awesome?",
+          :"db.install/_attribute" => :"db.part/db" },
+        { :"db/id" => Person.tempid(:"db.part/db"),
+          :"db/ident" => :"person/ssn",
+          :"db/valueType" => :"db.type/string",
+          :"db/cardinality" => :"db.cardinality/one",
+          :"db/unique" => :"db.unique/value",
+          :"db.install/_attribute" => :"db.part/db" },
+        { :"db/id" => Person.tempid(:"db.part/db"),
+          :"db/ident" => :"person/secret_name",
+          :"db/valueType" => :"db.type/string",
+          :"db/cardinality" => :"db.cardinality/one",
+          :"db/unique" => :"db.unique/identity",
+          :"db.install/_attribute" => :"db.part/db" },
+        { :"db/id" => Person.tempid(:"db.part/db"),
+          :"db/ident" => :"person/bio",
+          :"db/valueType" => :"db.type/string",
+          :"db/cardinality" => :"db.cardinality/one",
+          :"db/fulltext" => true,
+          :"db.install/_attribute" => :"db.part/db" }
+      ]
+    end
   end
 
   describe "in an instance" do
