903a65ef74d23be55c9adc8fed9afb3b8219530f
Ryan Neufeld <ryan@ryanneufeld.ca>
2012-11-15 17:36:16 -0500

    Config updates and #establish_base_connection. Doc updates.

--- lib/diametric/config/environment.rb
+++ lib/diametric/config/environment.rb
@@ -1,11 +1,10 @@
-# This is a word-for-word copy of Mongoid's lib/mongoid/config/environment.rb file
-#
-# Thanks!
-
 module Diametric
   module Config
 
     # Encapsulates logic for getting environment information.
+    #
+    # This is nearly a word-for-word copy of {http://github.com/mongoid/mongoid Mongoid}'s {https://github.com/mongoid/mongoid/blob/master/lib/mongoid/config/environment.rb lib/mongoid/config/environment.rb} file.
+    # Thanks!
     module Environment
       extend self
 
@@ -16,15 +15,13 @@ module Diametric
       # @example Get the env name.
       #   Environment.env_name
       #
-      # @raise [ Errors::NoEnvironment ] If no environment was set.
+      # @raise [ "No environment specified" ] If no environment was set.
       #
       # @return [ String ] The name of the current environment.
-      #
-      # @since 0.1.0
       def env_name
         return Rails.env if defined?(Rails)
         return Sinatra::Base.environment.to_s if defined?(Sinatra)
-        ENV["RACK_ENV"] || ENV["DIAMETRIC_ENV"] || raise(Errors::NoEnvironment.new)
+        ENV["RACK_ENV"] || ENV["DIAMETRIC_ENV"] || raise("No environment specified")
       end
 
       # Load the yaml from the provided path and return the settings for the
@@ -36,8 +33,6 @@ module Diametric
       # @param [ String ] path The location of the file.
       #
       # @return [ Hash ] The settings.
-      #
-      # @since 0.1.0
       def load_yaml(path, environment = nil)
         env = environment ? environment.to_s : env_name
         YAML.load(ERB.new(File.new(path).read).result)[env]
--- lib/diametric/persistence.rb
+++ lib/diametric/persistence.rb
@@ -1,25 +1,34 @@
 module Diametric
+  # Persistence is the main entry point for adding persistence to your diametric entities.
   module Persistence
-    extend self
+    autoload :Peer, 'diametric/persistence/peer'
+    autoload :REST, 'diametric/persistence/rest'
 
-    def connect(options)
-      mode = options.delete(:mode)
-      persistence_class(mode).connect(options)
+    # Establish a base connection for your application that is used unless specified otherwise. This method can
+    # establish connections in either REST or peer mode, depending on the supplied options.
+    #
+    # @example Connecting in peer-mode (JRuby required)
+    #   Diametric::Persistence.establish_base_connection({:uri => "datomic:free://localhost:4334/my-db"})
+    #
+    # @example Connecting in REST-mode
+    #   Diametric::Persistence.establish_base_connection({:uri => "http://localhost:9000/",
+    #                                                     :database => "my-db",
+    #                                                     :alias => "my-dbalias"})
+    def self.establish_base_connection(options)
+      @_persistence_class = persistence_class(options[:uri])
+      @_persistence_class.connect(options)
+    end
+
+    # Including +Diametric::Persistence+ after establishing a base connection will include
+    # the appropriate Persistence class ({REST} or {Peer})
+    def self.included(base)
+      base.send(:include, @_persistence_class) if @_persistence_class
     end
 
     private
 
-    def persistence_class(mode)
-      mode = mode.to_sym
-
-      case
-      when :rest
-        require 'diametric/persistence/rest'
-        Diametric::Persistence::REST
-      when :peer
-        require 'diametric/persistence/peer'
-        Diametric::Persistence::Peer
-      end
+    def self.persistence_class(uri)
+      uri =~ /^datomic:/ ? Peer : REST
     end
   end
 end
