3d19bb9377352e5bca916a50823925cae4d11506
Yoko Harada <yokolet@gmail.com>
2013-06-06 11:45:22 -0400

    Fixes to support more clojure keywords

--- lib/diametric/entity.rb
+++ lib/diametric/entity.rb
@@ -352,12 +352,12 @@ module Diametric
           return thing
         end
         first_key = entity.keys.first
-        match_data = /:([a-z0-9]+)\/([a-z0-9_]+)/.match(first_key)
+        match_data = /:([a-zA-Z0-9]+)\/([a-zA-Z0-9_]+)/.match(first_key)
         entity_name = match_data[1].capitalize
         instance = eval("#{entity_name}.new")
         instance.send("#{match_data[2]}=", entity[first_key])
         entity.keys[1..-1].each do |key|
-          match_data = /:([a-z0-9]+)\/([a-z0-9_]+)/.match(key)
+          match_data = /:([a-zA-Z0-9]+)\/([a-zA-Z0-9_]+)/.match(key)
           instance.send("#{match_data[2]}=", entity[key])
         end
         instance.send("dbid=", Diametric::Persistence::Object.new(dbid))
